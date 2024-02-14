523de85990cff620b40bfe82bc0f64cdb5c5027c
Mikhail Sazonov <wursta@gmail.com>
2024-02-14 22:43:13 +0300

    HW#2
    Оптимизировал алгоритм.
    Убрал перекопирование строки, если встретился 0:
    Теперь смотрю на "следующий" символ и если он 0, то не добавляю текущий.

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,39 +10,58 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
 	source := []rune(str)
+	sLen := len(source)
+
+	if len(source) == 0 {
+		return "", nil
+	}
+
+	// Если первый символ это число, то отдаём ошибку
+	_, isNum := getNum(source[0])
+	if isNum {
+		return "", ErrInvalidString
+	}
 
 	b := strings.Builder{}
 
 	for i, v := range source {
-		repeatCount, err := strconv.Atoi(string(v))
-		if err != nil {
-			b.WriteRune(v)
-			continue
-		}
+		repeatCount, isNum := getNum(v)
 
-		// Если мы тут, то это число, но если это первый элемент, то отдаём ошибку
-		if i == 0 {
-			return "", ErrInvalidString
+		if !isNum {
+			isLast := i == sLen-1
+			if isLast {
+				b.WriteRune(v)
+			} else {
+				// Если следующий символ - число и он равен 0, то пропускаем символ
+				nextNum, isNum := getNum(source[i+1])
+				if (isNum && nextNum > 0) || !isNum {
+					b.WriteRune(v)
+				}
+			}
+			continue
 		}
 
 		// Числа не могут идти друг за другом.
 		// Если предыдущее значение - это число, то отдаём ошибку.
-		_, err = strconv.Atoi(string(source[i-1]))
-		if err == nil {
+		_, isNum = getNum(source[i-1])
+		if isNum {
 			return "", ErrInvalidString
 		}
 
-		if repeatCount > 0 {
-			b.WriteString(strings.Repeat(string(source[i-1]), repeatCount-1))
-		} else {
-			// Если пришёл 0, то нужно пересобрать строку, убрав последний добавленный символ
-			tmpStr := []rune(b.String())
-			tmpStr = tmpStr[:len(tmpStr)-1]
-			b.Reset()
-			for _, tmpV := range tmpStr {
-				b.WriteRune(tmpV)
-			}
+		// Нули обрабатываются в блоке считывания букв
+		if repeatCount == 0 {
+			continue
 		}
+
+		b.WriteString(strings.Repeat(string(source[i-1]), repeatCount-1))
 	}
 	return b.String(), nil
+}
+
+func getNum(letter rune) (int, bool) {
+	num, err := strconv.Atoi(string(letter))
+	if err != nil {
+		return 0, false
+	}
+	return num, err == nil
 }
