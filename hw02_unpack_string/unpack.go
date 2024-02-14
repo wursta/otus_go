921995f946beccdca85852033b77d1e6b102b445
Mikhail Sazonov <wursta@gmail.com>
2024-02-14 11:01:33 +0300

    HW#2
    Исправление ошибки линтера: `if err != nil` has complex nested blocks (complexity: 5) (nestif)

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -24,29 +24,30 @@ func Unpack(str string) (string, error) {
 
 		if err != nil {
 			b.WriteRune(v)
+			continue
+		}
+
+		// Если предыдущего элемента нет, то это первый элемент и он число -> то отдаём ошибку
+		if i == 0 {
+			return "", ErrInvalidString
+		}
+
+		// Числа не могут идти друг за другом.
+		// Если предыдущее значение - это число отдаём ошибку.
+		_, err = strconv.Atoi(string(source[i-1]))
+		if err == nil {
+			return "", ErrInvalidString
+		}
+
+		if repeatCount > 0 {
+			b.WriteString(strings.Repeat(string(source[i-1]), repeatCount-1))
 		} else {
-			// Если предыдущего элемента нет, то это первый элемент и он число -> то отдаём ошибку
-			if i == 0 {
-				return "", ErrInvalidString
-			}
-
-			// Числа не могут идти друг за другом.
-			// Если предыдущее значение - это число отдаём ошибку.
-			_, err := strconv.Atoi(string(source[i-1]))
-			if err == nil {
-				return "", ErrInvalidString
-			}
-
-			if repeatCount > 0 {
-				b.WriteString(strings.Repeat(string(source[i-1]), repeatCount-1))
-			} else {
-				// Если пришёл 0, то нужно пересобрать строку, убрав последний добавленный символ
-				tmpStr := []rune(b.String())
-				tmpStr = tmpStr[:len(tmpStr)-1]
-				b.Reset()
-				for _, tmpV := range tmpStr {
-					b.WriteRune(tmpV)
-				}
+			// Если пришёл 0, то нужно пересобрать строку, убрав последний добавленный символ
+			tmpStr := []rune(b.String())
+			tmpStr = tmpStr[:len(tmpStr)-1]
+			b.Reset()
+			for _, tmpV := range tmpStr {
+				b.WriteRune(tmpV)
 			}
 		}
 
